5cc4b0cbfc38e29fc580438f7f9393d89320b742
Curtis Cooper <curtiscooper1@quickenloans.com>
2021-05-17 09:03:21 -0400

    lab files

---
+++ Lab_11_3_Coffee_Registration/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Lab_11_3_Coffee_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_11_3_Coffee_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowRegistration()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayProfile(User customer)
+        {
+            if (ModelState.IsValid == true)
+            {
+                return View(customer);
+            }
+
+            return Content("ErrorMessage");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab_11_3_Coffee_Registration/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_11_3_Coffee_Registration.Models
+{
+    public class User
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Birthday { get; set; }
+        [Required]
+        public string FavoriteDrink { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match, try again.")]
+        public string ConfirmPassword { get; set; }
+
+        
+    }
+}
